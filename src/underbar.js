a205ce60de14560a6d226cb3cc1ae57ebacd7033
mishroom <mishall.afzal@gmail.com>
2017-11-28 15:16:30 -0800

    extend

--- src/underbar.js
+++ src/underbar.js
@@ -258,7 +258,15 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
+  _.extend = function(obj, ...args) {
+    _.each(args, function (nestedObj) {
+      _.each(nestedObj, function (item, key, object) {
+        obj[key] = item;
+      });
+    });
+    
+    
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
